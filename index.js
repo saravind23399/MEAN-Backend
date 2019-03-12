3c11708cc880f54643b0867c1de31bdfc0639dff
saravind23399 <saravind23399@gmail.com>
2019-03-12 14:51:00 +0530

    Changes
    Aravind S

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const cors = require('cors')
+// Importing the requirements
+ const express = require('express')
+ const mongoose = require('mongoose')
+ const cors = require('cors')
+ const bodyparser = require('body-parser')
+
+// Declarations
+const app = express()
+
+// Test route{
+app.get('/helloworld', (req, res)=>{
+    res.send("<strong>Hello Everyone</strong>")
+})
+
+app.get('/display/:name' ,(req,res)=>{
+    res.send(req.params.name)
+})
+var PORT = process.env.PORT || 62000
+//Starting the Server
+app.listen(PORT, ()=>{
+    console.log("Server started at port, " + PORT)
+})
