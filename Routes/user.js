66e1d33012965c4667da77042b504e80a102910a
saravind23399 <saravind23399@gmail.com>
2019-03-12 16:51:42 +0530

    Changes
    Aravind S

--- Routes/user.js
+++ Routes/user.js
@@ -5,28 +5,42 @@ router.get('/test',(req, res)=>{
     res.send("User Route")
 })
 
-module.exports = router
+router.post('/createUser', (req, res)=>{
+    var newUser = user({
+        username : req.body.user,
+        password: req.body.password
+    })
 
+    newUser.save((saveError, saveDocs)=>{
+        res.send('Success')
+    })
+})
 
-// router.post('/authenticate', (req, res) => {
-//     user.find({username: req.body.username}, (findErr, findDocs)=>{
-//         if(findDocs.length == 0){
-//             res.json({
-//                 success: false,
-//                 user: 'No User Found'
-//             })
-//         } else {
-//             res.json({
-//                 success: true,
-//                 user: findDocs[0].username
-//             })
-//         }
-//     })
-// })
+router.get('/getUsers' , (req, res)=>{
+    user.find({}).exec((err, docs)=>{
+        res.json(docs)
+    })
+})
+
+router.post('/authenticate', (req, res) => {
+    user.find({username: req.body.username}, (findErr, findDocs)=>{
+        if(findDocs.length == 0){
+            res.json({
+                success: false,
+                user: 'No User Found'
+            })
+        } else {
+            res.json({
+                success: true,
+                user: findDocs[0].username
+            })
+        }
+    })
+})
  
-// router.post('/create', (req, res) => {
+router.post('/create', (req, res) => {
 
-// })
+})
 
 // router.post('/delete', (req, res) => {
 
@@ -36,4 +50,4 @@ module.exports = router
 
 // })
 
-// module.exports = router
+module.exports = router
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  const mongoose = require('mongoose')
  const cors = require('cors')
  const bodyparser = require('body-parser')
-
+ const userRoute = require('./Routes/user')
 // Declarations
 const app = express()
 
@@ -12,8 +12,12 @@ app.use(cors({origin: 'http://localhost:3000'}))
 
 // BodyParser
 app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
 
-// Test route{
+// Test route
+
+app.use('/users', userRoute)
+
 app.get('/helloworld', (req, res)=>{
     res.send("<strong>Hello Everyone</strong>")
 })
@@ -24,11 +28,5 @@ app.get('/display/:name' ,(req,res)=>{
 var PORT = process.env.PORT || 3000
 //Starting the Server
 app.listen(PORT, ()=>{
-    mongoose.connect('mongodb://localhost:27017/meanworkshop', (err)=>{
-        if(err){
-            console.log("Cannot connect to DB")
-        } else {
-            console.log('Connected to DB')
-        }
-    })
+    mongoose.connect('mongodb://localhost:27017/meanworkshop')
 })
